dfd3e69bc17658d4bae4de4ec54d354b2cc3836e
Viktor Yakubiv <viktor.yakubiv@beetroot.se>
2018-01-27 16:50:30 +0200

    Memory Pair Game business logic

---
+++ memory-pair-game/card.js
@@ -0,0 +1,48 @@
+export default class Card {
+  static render({ value, onClick }, parentElement) {
+    const element = document.createElement('div')
+    element.className = 'card'
+
+    element.innerHTML = `
+      <span>${value}</span>
+    `
+    element.addEventListener('click', onClick)
+
+    if (parentElement) parentElement.appendChild(element)
+
+    return element
+  }
+
+  constructor(value, element, { onClick } = {}) {
+    this.value = value
+    this.onClick = onClick
+
+    this.element = Card
+      .render({ value, onClick: this.toggle.bind(this) }, element)
+  }
+
+  get opened() {
+    return this.element.classList.contains('opened')
+  }
+
+  set opened(value) {
+    this.element.classList.toggle('opened', value)
+  }
+
+  get hidden() {
+    return this.element.classList.contains('hidden')
+  }
+
+  set hidden(value) {
+    this.element.classList.toggle('hidden', value)
+  }
+
+  toggle() {
+    this.opened = !this.opened
+    if (this.onClick) this.onClick(this)
+  }
+
+  valueOf() {
+    return this.value
+  }
+}
